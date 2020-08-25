9ea84ed5e57f30f194da1d5afb180ae7d87c3c6a
Gildas <gildas.lormeau@gmail.com>
2020-08-25 15:00:30 +0200

    updated build script

--- build-extension.sh
+++ build-extension.sh
@@ -3,12 +3,12 @@ rm screenbreak-extension-firefox.zip screenbreak-extension-chromium.zip screenbr
 cp manifest.json manifest.copy.json
 
 jq "del(.options_page,.background.persistent,.permissions[0])" manifest.copy.json > manifest.json
-zip -r screenbreak-extension-firefox.zip manifest.json common extension lib _locales
+zip -r screenbreak-extension-firefox.zip manifest.json extension lib _locales
 
 jq "del(.browser_specific_settings,.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
-zip -r screenbreak-extension-chromium.zip manifest.json common extension lib _locales
+zip -r screenbreak-extension-chromium.zip manifest.json extension lib _locales
 
 jq "del(.browser_specific_settings,.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
-zip -r screenbreak-extension-edge.zip manifest.json common extension lib _locales
+zip -r screenbreak-extension-edge.zip manifest.json extension lib _locales
 
 mv manifest.copy.json manifest.json
