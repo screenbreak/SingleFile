2bc471b394f018fef4da31a0ef47efae4324b003
Gildas <gildas.lormeau@gmail.com>
2020-08-27 13:14:01 +0200

    hide options page

--- build-extension.sh
+++ build-extension.sh
@@ -2,13 +2,13 @@
 rm screenbreak-extension-firefox.zip screenbreak-extension-chromium.zip screenbreak-extension-edge.zip
 cp manifest.json manifest.copy.json
 
-jq "del(.options_page,.background.persistent,.permissions[0])" manifest.copy.json > manifest.json
+jq "del(.background.persistent,.permissions[0])" manifest.copy.json > manifest.json
 zip -r screenbreak-extension-firefox.zip manifest.json extension lib _locales
 
-jq "del(.browser_specific_settings,.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[1])" manifest.copy.json > manifest.json
 zip -r screenbreak-extension-chromium.zip manifest.json extension lib _locales
 
-jq "del(.browser_specific_settings,.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[1])" manifest.copy.json > manifest.json
 zip -r screenbreak-extension-edge.zip manifest.json extension lib _locales
 
 mv manifest.copy.json manifest.json
