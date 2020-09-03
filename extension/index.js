8fa7f849a85ff35eea3c09e4dbf6bfbc89b706e9
Gildas <gildas.lormeau@gmail.com>
2020-09-03 21:14:29 +0200

    Removed unused code

--- extension/index.js
+++ extension/index.js
@@ -1,7 +1,5 @@
 this.screenbreak = this.screenbreak || {
 	extension: {
-		injectScript: (tabId, options) => this.screenbreak.extension.lib.core.bg.scripts.inject(tabId, options),
-		getPageData: (options, doc, win, initOptions = { fetch: this.screenbreak.extension.lib.fetch.content.resources.fetch }) =>
-			this.screenbreak.lib.getPageData(options, initOptions, doc, win)
+		injectScript: (tabId, options) => this.screenbreak.extension.lib.core.bg.scripts.inject(tabId, options)
 	}
 };
