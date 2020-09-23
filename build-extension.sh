f9a9693ab40589ec176e4d569923cbd6d3588110
Gildas <gildas.lormeau@gmail.com>
2020-09-23 17:24:43 +0200

    Fix typo

--- build-extension.sh
+++ build-extension.sh
@@ -8,7 +8,7 @@ zip -r screenbreak-extension-firefox.zip manifest.json extension lib _locales
 jq "del(.browser_specific_settings,.permissions[1],.permissions[2])" manifest.copy.json > manifest.json
 zip -r screenbreak-extension-chromium.zip manifest.json extension lib _locales
 
-jq "del(.browser_specific_settings,.permissions[1]),.permissions[2]" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[1],.permissions[2])" manifest.copy.json > manifest.json
 zip -r screenbreak-extension-edge.zip manifest.json extension lib _locales
 
 mv manifest.copy.json manifest.json
